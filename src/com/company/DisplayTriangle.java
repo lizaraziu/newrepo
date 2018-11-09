578a163ed875bea497517764f72c8ec7db6a330c
Elizar <sam@example.com>
2018-11-09 13:43:54 +0200

    about luckyTicket

--- src/com/company/DisplayTriangle.java
+++ src/com/company/DisplayTriangle.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class DisplayTriangle {
+
+    public static void main(String[] args) {
+        Triangles triangle = InputController.createTriangle();
+
+        OutputController.printAreaAndPerimeter(triangle);
+
+        Patrat patrat = InputController.creatPatrat();
+        OutputController.printAreaAndPerimeter(patrat);
+
+        Cercale circle = InputController.creatCercale();
+        OutputController.printAreaAndPerimeter(circle);
+
+
+
+    }
 }
