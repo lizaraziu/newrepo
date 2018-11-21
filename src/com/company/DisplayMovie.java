36f65cb9e042b5b30f569a5d1d886f37b5246969
Elizar <sam@example.com>
2018-11-21 12:54:38 +0200

    double test

--- src/com/company/DisplayMovie.java
+++ src/com/company/DisplayMovie.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import javafx.scene.transform.MatrixType;
+
 public class DisplayMovie {
-}
+
+    public static void main(String[] args) {
+
+            Movie movie1 = new Movie("The Godfather");
+            Movie movie2 = new Movie("Avengers");
+            Movie movie3 = new Movie("Shark");
+            Movie movie4 = new Movie("Yes Man");
+            Movie movie5 = new Movie("Gladiator");
+
+            Movie[] movies = {movie1, movie2, movie3, movie4, movie5};
+
+            MoviesController.likeRandomly(movies, 15);
+            MoviesController.print(movies);
+
+        }
+    }
+
